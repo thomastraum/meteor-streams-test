766a653410d125f431c3a2461cfc7479082ea42e
Thomas Eberwein <thomas@thomastraum.com>
2020-07-11 01:40:54 +0100

    mouse move. started building it as a mouse class

--- client/main.js
+++ client/main.js
@@ -1,41 +1,36 @@
 import { Notifications } from '/api/notifications.js';
+import { mouseSprite } from './mousesprite';
 import './main.html';
 
-// getimage[0].style.transform = "translateX(0," + val + ")";
-// translate with CSS
 function moveTo(x,y) {
-  $("#thesprite").css({
-    top: y,
-    left: x,
-    position:'absolute'
-  });
+  document.getElementById("thesprite").style.transform = `translate(${x}px,${y}px)`;
 }
 
 //listen on notifications on the message event
-Notifications.on('message', function(message, time) {
-  console.log(this.userId,this.subscriptionId);
-  moveTo(message.x,message.y);
-  var completeMessage = `${message.x} : ${message.y} @${new Date(time).toString()}`;
-  $('#messages').prepend('<div>' + completeMessage + '</div>');
+Notifications.on('message', function(message) {
+  // console.log(this.userId,this.subscriptionId);
+
+  moveTo( message.x * $('body').width(), message.y *  $('body').height() );
+  // var completeMessage = `${message.x} : ${message.y}`;
+  // $('#messages').prepend('<div>' + completeMessage + '</div>');
 });
 
-// setInterval(function() {
-//   console.log("asda");
-//   Notifications.emit('message',{
-//     x:Math.random()*200,
-//     y:Math.random()*200,
-//   }, Date.now());
-// }, 1000);
+
+Template.body.rendered = function(){
+  myMouse1 = new mouseSprite();
+};
 
 //simple clear message action
 Template.body.events({
+  'mousemove ' : function(e) {
+    const percentOffsetX = e.offsetX /  $('body').width();
+    const percentOffsetY = e.offsetY /  $('body').height();
 
-  'click #randomize-pos': function() {
-    // console.log('clicked');
+// console.log(percentOffsetX);
     Notifications.emit('message',{
-      x:Math.random()*200,
-      y:Math.random()*200,
-    }, Date.now());
+      x : percentOffsetX,
+      y : percentOffsetY,
+    });
   },
   'click #clear-messages': function() {
     $('#messages').html('');
---
+++ client/mousesprite.js
@@ -0,0 +1,21 @@
+class mouseSprite {
+  constructor(id) {
+    this.id = id;
+    this.createSprite();
+  }
+
+  createSprite(){
+    this.sprite = document.createElement("div");
+    this.sprite.setAttribute("id", "thesprite"); //this.id);
+    this.sprite.setAttribute("class", "sprite");
+    document.body.appendChild(this.sprite);
+    console.log(this.sprite);
+  }
+
+  moveSprite(x,y) {
+    this.sprite.style.transform = `translate(${x}px,${y}px)`;
+  }
+
+}
+
+export { mouseSprite };
